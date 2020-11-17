f4f9fbebd7306c378d4e3907706c0b77eee8e175
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2020-11-16 21:29:37 -0500

    install cropperjs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ImageCropperComponent } from './image-cropper/image-cropper.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageCropperComponent
   ],
   imports: [
     BrowserModule
